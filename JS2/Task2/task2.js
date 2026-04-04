03201971ee1c30f7171cda3014141fe0324f3dcb
yuvraj006-byte <yuvivermasep006@gmail.com>
2026-04-04 16:50:26 +0300

    Task2 Updated

--- JS2/Task2/task2.js
+++ JS2/Task2/task2.js
@@ -2,12 +2,15 @@ const numParticipant = parseInt(prompt("Enter the number of Participants"));
 
 let nameList = [];
 for (let i = 0; i < numParticipant; i++) {
-    const names = prompt("Enter the Names of the Participants");
-    nameList.push(names);
-};
+  const names = prompt("Enter the Names of the Participants");
+  nameList.push(names);
+}
 
-const listItem  = document.getElementById("list")
+nameList.sort();
+
+const listItem = document.getElementById("list");
 listItem.innerHTML = "";
+
 for (const names of nameList) {
-    listItem.innerHTML += `<li>${names}</li>` ;
+  listItem.innerHTML += `<li>${names}</li>`;
 }
